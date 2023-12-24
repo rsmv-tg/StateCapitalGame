716a4aa519863c4648b3bfce9f2d98b42b96b4dd
rsmvdev <rsmvstudios@protonmail.com>
2023-12-23 19:09:46 -0500

    Basic Game Mechanics Complete

--- js/StateCapitalsGame.js
+++ js/StateCapitalsGame.js
@@ -1,4 +1,4 @@
-//javaScript Portfolio Project. Goal is to have user select teh proper capital of a randomply generated state. If the user selects the correct answer the corrects state will be displayed in text along with the location on an interactive map (ex. Google API)
+//javaScript Portfolio Project. Goal is to have user select the proper capital of a randomply generated state. If the user selects the correct answer the corrects state will be displayed in text along with the location on an interactive map (ex. Google API)
 
 const STATES = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
@@ -18,53 +18,82 @@ const CAPITAL_ZIPCODES = ['36101', '99801', '85001', '72201', '95814', '80201', 
 '87501', '12201', '27601', '58101', '43201', '73101', '97301', '17101', '02901', '29201',
 '57101', '37201', '73301', '84101', '05601', '23218', '98501', '25301', '53701', '82001']
 
-function runGame() {
-
     function runGame() {
 
-        const sourceIndex = Math.floor(Math.random() * STATES.length);
-        const targetIndex = Math.floor(Math.random() * STATES.length);
-        const sourceState = STATES[sourceIndex];
-        const targetState = STATES[targetIndex];
-        const targetCapital = STATE_CAPITALS[targetIndex];
-        const targetZip = CAPITAL_ZIPCODES[targetIndex]
+        const targetIndex = Math.floor(Math.random() * STATES.length); //Randomizes the states in states to generate the target index
+        const targetState = STATES[targetIndex]; //Logs the randomized indexed state in STATES
+        const targetCapital = STATE_CAPITALS[targetIndex]; //Aligns the target state in STATES against the appropriate index in STATE_CAPITALS
+        const targetZipCode = CAPITAL_ZIPCODES[targetIndex] //Aligns the target state in STATES against the appropriate index in CAPITAL_ZIPCODES
     
-        console.log("Source state: " + sourceState);
-        console.log("Target state: " + targetState);
-        console.log("Target capital: " + targetCapital);
-    
-        let guess = " ";
+        console.log(`Target state is: ${targetState}.`);
+        console.log(`Target capital is: ${targetCapital}.`);
+        console.log(`Target Zip Code is: ${targetZipCode}.`);
+
+//// Good - 12/23/2023
+
+
+        let attempt = " ";
         let correct = false;
-        let guessCount = 0;
+        let attemptCount = 0;
 
-    console.log("This is the target " + targetState);
-    do {
-        guess = prompt('I am thinking of one of these colors:\n\n' + COLORS_ARRAY.join(", ") +
-            '\n\nWhat color am I thinking of?\n');
-        guessCount++;
-        if (guess === null) {
-            return;
+        do {
+
+            attempt = prompt(`What is the capital of ${targetState}?`);
+
+            attemptCount++;
+
+            if (attempt === null) {
+
+                alert(`Thank you for playing. The capital of ${targetState} is ${targetCapital}.`)
+
+                return;
+
+            }
+
+            correct = checkAttempt(attempt, targetCapital);
+
         }
-        correct = checkGuess(guess, target);
 
-    } while (!correct);
-    alert(`Congratulations! You guessed the correct color (${target}) in ${guessCount} guesses.`);
-    document.body.style.background = guess;
-}
+        while (!correct);
 
-function checkGuess(guess, target) {
-    let correct = false;
-    if (!COLORS_ARRAY.includes(guess)) {
-        alert("The guess wasn't from the option,please choose from provided colors");
+        if (attemptCount === 1) {
+
+            alert(`Congratulations! You successfully identified ${targetCapital} as the capital of ${targetState} on your first try. Awesome!`);
+
+        } else if (attemptCount <= 3) {
+
+            alert(`Congratulations! You successfully identified ${targetCapital} as the capital of ${targetState} in just ${attemptCount} attempts. Great Job!`);
+
+        } else {
+
+            alert(`Congratulations! You successfully identified ${targetCapital} as the capital of ${targetState} in ${attemptCount} attempts. Thank you for playing.`);
+        }
+        
+        //alert(`Congratulations! You successfully identified ${targetCapital} as the capital of ${targetState} in just ${attemptCount} attempts.`);
+
+
+        ///Code to tie in map input should go here///
+    
+      
+        function checkAttempt(attempt, targetCapital) {
+
+        let correct = false;
+
+        if (!STATE_CAPITALS.includes(attempt)) {
+
+            alert(`Sorry, ${attempt} is not a recognized capital city within the United States. Please try again`)
+
+        } else if (attempt !== targetCapital) {
+
+            alert(`While ${attempt} is a capital city in the United States, it's not the capital of ${targetState}. Please try again.`)
+
+        } else {
+
+            correct = true
+
+        }
+
+        return correct;
+
+        }
     }
-    else if (guess > target) {
-        alert("Sorry, Please try again.")
-    }
-    else if (guess < target) {
-        alert("Sorry, Please try again.")
-    }
-    else {
-        correct = true;
-    }
-    return correct;
-}
